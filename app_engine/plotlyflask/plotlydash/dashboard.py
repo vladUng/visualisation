9e07e31e88f6875934aa1dec3b44322ca6d72bf9
vladUng <vlad2403@gmail.com>
2020-09-28 22:32:20 +0300

    Added code to put all the data in a dataset and save it to a .tsv file
    Removed samples option from metadata
    
    Add selected all or none datasets

--- app_engine/plotlyflask/plotlydash/dashboard.py
+++ app_engine/plotlyflask/plotlydash/dashboard.py
@@ -9,6 +9,8 @@ from dash.dependencies import Input, Output, State
 import plotly.express as px
 import plotly.graph_objects as go
 
+from os import path
+
 import time 
 
 def process_metadata(df_metadata):
@@ -20,38 +22,64 @@ def process_metadata(df_metadata):
 
     return df_metadata
 
-def import_files(base_path):
+def import_data(base_path):
+    """Creates the dataframe used for the tool and the metadata
+
+    Args:
+        base_path ([String]): Path to the data
+
+    Returns: 
+        DataDict: A dictionary which contains the the all_tsv and metadata 
+    """
+    start_time = time.time()
+    if path.exists(base_path):
+        ret_dict = {}
+        ret_dict["all_tsv"] = pd.read_csv(base_path + "all_data.tsv", delimiter="\t")
+        raw_df = pd.read_csv(base_path + "00_JBU_sequencing_metadata.tsv", delimiter="\t")
+        ret_dict["metadata"] = process_metadata(raw_df)
+        print("Finished loading the data in {}".format(time.time()-start_time))
+        return ret_dict
+    else:
+        return None
+
+def update_all_datasets(base_path):
+    """ 
+    Function which takes the names of all the files from the master.tsv files and then updates the all tsv file
+
+    Args:
+        base_path ([String]): Path to /data folder 
+
+    Returns:
+        [type]: [description]
+    """
+    start_time = time.time()
     # open the master .csv file, containing the contents of the folder
     master_cv = pd.read_csv(base_path + "master.tsv", delimiter="\t")
 
     # iterate through the csv file and create a list of dictionaries with the following: <key> - name of the file, <value> object which contains a dataframe and description from the .csv file.
+    all_tsv, metadata = pd.DataFrame(), pd.DataFrame()
     ret_dict = {}
-    all_tsv = pd.DataFrame()
-    combined_genes = pd.DataFrame() 
     for index, row in master_cv.iterrows():
         filename, description = row["Filename"], row["Description"]
         df = pd.read_csv(base_path+filename, delimiter="\t")
         print("Dataset: {} Shape {}".format(filename, df.shape))
-        ret_dict[filename] = { "df": df, "description": description }
     
         if "metadata" not in filename:
             if not all_tsv.empty:
                 all_tsv = pd.merge(all_tsv, df, how="outer")
             else:
                 all_tsv = df
+        else:
+            metadata = df 
 
-        # get gene column, not sure if it's still used
-        if "genes" in df.columns.values:
-            genes = pd.DataFrame(df["genes"])
-            genes["dataset"] = filename
-            combined_genes = combined_genes.append(genes)
+    print("Finished loading the data in {}".format(time.time()-start_time))
+    ret_dict["metadata"] = process_metadata(metadata)
+    ret_dict["all_tsv"] = all_tsv
 
-    ret_dict["00_JBU_sequencing_metadata.tsv"]["df"] = process_metadata(ret_dict["00_JBU_sequencing_metadata.tsv"]["df"])
-    ret_dict["all_tsv"] = {
-        "df": all_tsv,
-        "description": "all tsv files in one place"
-    }
-    return ret_dict, combined_genes
+    start_time = time.time()
+    all_tsv.to_csv(base_path + "all_data.tsv",  sep='\t', index = False)
+    print("Update .tsv file with the new data in {}".format(time.time() - start_time))
+    # return ret_dict
     
 def create_datasets_plot(found_in, metadata, user_input, datasets_selected):
     if not found_in.empty:
@@ -78,7 +106,7 @@ def create_datasets_plot(found_in, metadata, user_input, datasets_selected):
         return "Gene {} not found in any of the datasets".format(user_input), {}   
 
 def create_medata_plot(found_in, metadata, user_input, datasets_selected,
-                        samples, tissue, diagnosis, substrate, ter, gender, xaxis):
+                            tissue, diagnosis, substrate, ter, gender, xaxis):
     if not found_in.empty:
         ret_str = 'Gene {} was found'.format(user_input)
         # create the figure layout
@@ -93,9 +121,9 @@ def create_medata_plot(found_in, metadata, user_input, datasets_selected,
         metadata_selected = filter_data(metadata, "Dataset", datasets_selected)
         metadata_selected = filter_data(metadata_selected, "Tissue", tissue)
         metadata_selected = filter_data(metadata_selected, "Substrate", substrate)
-
         metadata_selected = filter_data(metadata_selected, "Gender", gender)
 
+        # synchronize the metadata df with the tpm dataset
         found_in = filter_data(found_in, "Sample", list(metadata_selected["Sample"].values))
         metadata_selected= metadata_selected.sort_values("Sample")
         found_in = found_in.sort_values("Sample")
@@ -118,26 +146,33 @@ def create_dfs_tick_box(data_dict):
     # itereate through each of the dictionary and create the html code
     # add calbacks 
     options = []
-    datasets = data_dict["00_JBU_sequencing_metadata.tsv"]["df"]["Dataset"].unique()
+    datasets = data_dict["metadata"]["Dataset"].unique()
     for dataset in datasets:
         options.append({
             "label": dataset,
             "value": dataset,
         })
     return html.Div([
-        html.H6("Change the value in the text box to see callbacks in action!"),
+        html.H4("Change the value in the text box to see callbacks in action!"),
         dcc.Checklist(
             id = "data-checklist",
             options = options,
             value = datasets,
             style = { "column-count": "2"}),
         html.Br(),
+        dcc.RadioItems(
+            id = "select-all-none",
+            options= [
+                {'label': 'All datasets', 'value': 'all'},
+                {'label': 'None datasets', 'value': 'none'},
+            ]
+        ),
     ])
 
 def create_gene_search(data_dict):
     # column names should be the same as the ones from the dataframe containing the gen
     return html.Div(id="gene-controls", children = [
-                html.H6("Find a gene in the dataset"),
+                html.H3("Find a gene in the dataset"),
                 html.Div([
                     "Gene name: ",
                     dcc.Input(id='gene-input', value="", type='text')]),
@@ -158,13 +193,7 @@ def metadata_menu(metadata_df):
 
     return html.Div(id="metadata-menu", style={"column-count":"1", "margin-top": "40%"},
      children=[
-        html.H6("Select sample(s)"),
-        html.Label('Select Sample'),
-        dcc.Dropdown(
-            id="sample-dropdown", value=[metadata_df["Sample"].dropna().unique()[0]], multi=True,
-            options=[{"label": i, "value": i} for i in metadata_df["Sample"]]
-        ),
-        html.Br(),
+        html.H5("Metadata panel"),
         html.Label('Choose Tissue'),
         dcc.Dropdown(
             id="tissue-dropdown", value=[metadata_df["Tissue"].dropna().unique()[0]], multi=True,
@@ -207,26 +236,43 @@ def metadata_menu(metadata_df):
             id="xaxis-dropdown", value="Tissue",
             options=[{"label": i, "value": i} for i in ["NHU_differentiation", "Tissue", "Gender", "Diagnosis", "Substrate", "Dataset"]]
         ), 
+        html.Label('Curate the list'),
+        dcc.Checklist(
+            id="curate-list",
+            options= [
+                {'label': 'All datasets', 'value': 'all'},
+                {'label': 'None datasets', 'value': 'none'},
+            ]
+        ),
+        html.Label('Test'),
+        dcc.RadioItems(
+            id = "test-radio",
+            options= [
+                {'label': 'All datasets', 'value': 'all'},
+                {'label': 'None datasets', 'value': 'none'},
+            ]
+        ),
         # html.Br(),
         # html.Label('Choose plot colouring'),
         # dcc.Dropdown(
         #     id="colouring-dropdown", value="Dataset",
         #     options=[{"label": i, "value": i} for i in ["NHU_differentiation", "Tissue", "Gender", "Diagnosis", "Substrate", "Dataset"]]
         # ), html.Br(),
-        html.Label("After you selected the configuration from above press the below button to plot"),
+        html.Label("After you selected the configuration from above press the below button to plot"), 
+        html.Br(),
         html.Button(id='metadata-plot', n_clicks=0, children='Plot'),
     ])
         
-def init_callbacks(dash_app, data_dict, combined_genes):
+def init_callbacks(dash_app, data_dict):
     @dash_app.callback(
          [Output('search-result', "children"), Output('dataset-plots', "figure")],
          [Input('plot-gene', 'n_clicks'), Input('metadata-plot', 'n_clicks')],
          [State("gene-input", "value"),  State("data-checklist", "value"),
-         State("sample-dropdown","value"), State("tissue-dropdown","value"), State("diagnosis-dropdown","value"),
+         State("tissue-dropdown","value"), State("diagnosis-dropdown","value"),
          State("substrate-dropdown","value"), State("ter-range","value"), State("gender-radiobtn","value"), State("xaxis-dropdown", "value")]
     )
     def button_router(btn_1, btn_2, user_input, datasets_selected, 
-                        samples, tissue, diagnosis, substrate, ter, gender, xaxis):
+                        tissue, diagnosis, substrate, ter, gender, xaxis):
         ctx = dash.callback_context
 
         # get the button_id
@@ -237,8 +283,8 @@ def init_callbacks(dash_app, data_dict, combined_genes):
 
         prcsd_str = user_input.strip()
         if prcsd_str:
-            all_tsv = data_dict["all_tsv"]["df"]
-            metadata = data_dict["00_JBU_sequencing_metadata.tsv"]["df"]
+            all_tsv = data_dict["all_tsv"]
+            metadata = data_dict["metadata"]
             found_in = all_tsv[all_tsv["genes"].str.match("\\b"+prcsd_str+"\\b", case = False, na=False)]
 
             if button_id == "plot-gene":
@@ -248,10 +294,20 @@ def init_callbacks(dash_app, data_dict, combined_genes):
             else:
                 print("Metadata plot has been triggered")
                 ret_string, fig = create_medata_plot(found_in, metadata, prcsd_str, datasets_selected, 
-                                                        samples, tissue, diagnosis, substrate, ter, gender, xaxis)
+                                                        tissue, diagnosis, substrate, ter, gender, xaxis)
                 return ret_string, fig
 
         return "Search for gene and click submit", {}
+
+    @dash_app.callback(
+        Output("data-checklist", "value"),
+        [Input("select-all-none", "value")]
+    )
+    def select_all_none_dasets(radio_value):    
+        if radio_value == "none":
+            return []
+        else:
+            return list(data_dict["metadata"]["Dataset"].unique())
 
     @dash_app.callback(
     Output('output-container-range-slider', 'children'),
@@ -267,8 +323,10 @@ def init_dashboard(server):
         routes_pathname_prefix='/dashapp/',
         external_stylesheets=external_stylesheets
     )
-    data_dict, combined_genes = import_files("/Users/vlad/Documents/Code/York/visualisation/data/")
-    init_callbacks(dash_app, data_dict, combined_genes)
+
+    # update_all_datasets("/Users/vlad/Documents/Code/York/visualisation/data/")
+    data_dict = import_data("/Users/vlad/Documents/Code/York/visualisation/data/")
+    init_callbacks(dash_app, data_dict)
 
     # Create Layout
     dash_app.layout = html.Div(children=[
@@ -277,7 +335,7 @@ def init_dashboard(server):
             html.Div(id="parent-controllers", style = { "column-count": "2", "width":"fit-content"}, 
             children=[
                 create_gene_search(data_dict), 
-                metadata_menu(data_dict["00_JBU_sequencing_metadata.tsv"]["df"]),
+                metadata_menu(data_dict["metadata"]),
             ]),
             dcc.Graph(
                 id='dataset-plots',
