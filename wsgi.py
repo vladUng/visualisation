d680bd5abbf50cfae9b2797dcea0805e080d25f6
vladUng <vlad2403@gmail.com>
2020-09-30 20:01:10 +0100

    Added code that used for g-cloud/ app engine

--- wsgi.py
+++ wsgi.py
@@ -10,5 +10,5 @@ def open_browser():
       webbrowser.open_new('http://127.0.0.1:8080/dashapp')
       
 if __name__ == "__main__":
-    # Timer(1, open_browser).start()
+#     Timer(1, open_browser).start()
     app.run(host='0.0.0.0', port=8080, debug=True)
